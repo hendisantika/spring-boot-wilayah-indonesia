b0734768b2a6dfdd7c0b3b3a029ce4b2c2f6e22e
Hendi Santika <hendisantika@gmail.com>
2022-04-06 06:01:46 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringBootWilayahIndonesiaApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWilayahIndonesiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWilayahIndonesiaApplication.class, args);
+    }
+
+}
