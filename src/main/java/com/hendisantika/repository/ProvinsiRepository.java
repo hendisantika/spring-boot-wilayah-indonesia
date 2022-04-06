d78ba1d26264e2ade66b99ee1d0a7ec021cda48e
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:14:03 +0700

    #15 - Add ProvinsiRepository

---
+++ src/main/java/com/hendisantika/repository/ProvinsiRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Provinsi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-wilayah-indonesia
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/04/22
+ * Time: 07.13
+ */
+public interface ProvinsiRepository extends JpaRepository<Provinsi, String> {
+}
