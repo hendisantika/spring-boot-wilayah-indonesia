d43ac45b9684a87980516fc85f9f22a37738b643
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:15:58 +0700

    #18 - Add KelurahanRepository

---
+++ src/main/java/com/hendisantika/repository/KelurahanRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Kelurahan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-wilayah-indonesia
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/04/22
+ * Time: 07.15
+ */
+public interface KelurahanRepository extends JpaRepository<Kelurahan, String> {
+}
