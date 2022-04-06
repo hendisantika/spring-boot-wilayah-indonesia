cfa5df49e22d6325de6e50ba1b34b1c5ad83b2be
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:08:19 +0700

    #13 - Add Kelurahan Entity

--- src/main/java/com/hendisantika/entity/Kelurahan.java
+++ src/main/java/com/hendisantika/entity/Kelurahan.java
@@ -8,29 +8,23 @@ import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 @Entity
-@Table(name = "kecamatan")
+@Table(name = "kelurahan")
 @Data
-public class Kecamatan {
+public class Kelurahan {
     @Id
     @Column(name = "id", nullable = false, length = 36)
     private String id;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_kota")
-    private Kota idKota;
+    @JoinColumn(name = "id_kecamatan")
+    private Kecamatan idKecamatan;
 
     @Column(name = "kode", nullable = false, length = 50)
     private String kode;
 
     @Column(name = "nama", nullable = false)
     private String nama;
-
-    @OneToMany(mappedBy = "idKecamatan")
-    private Set<Kelurahan> kelurahans = new LinkedHashSet<>();
 
 }
