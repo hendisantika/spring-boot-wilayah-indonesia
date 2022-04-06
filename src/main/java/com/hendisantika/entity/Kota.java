7fbaf32435d4012c10a215197d5ca051c3ead41a
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:07:14 +0700

    #11 - Add Kota Entity

---
+++ src/main/java/com/hendisantika/entity/Kota.java
@@ -0,0 +1,36 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "kota")
+@Data
+public class Kota {
+    @Id
+    @Column(name = "id", nullable = false, length = 36)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_provinsi", nullable = false)
+    private Provinsi idProvinsi;
+
+    @Column(name = "kode", nullable = false, length = 50)
+    private String kode;
+
+    @Column(name = "nama", nullable = false)
+    private String nama;
+
+    @OneToMany(mappedBy = "idKota")
+    private Set<Kecamatan> kecamatans = new LinkedHashSet<>();
+}
