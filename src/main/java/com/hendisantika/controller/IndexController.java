84f4f00471651b5abab716babfe84e9caf76e2c9
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:19:19 +0700

    #19 - Add IndexController

---
+++ src/main/java/com/hendisantika/controller/IndexController.java
@@ -0,0 +1,30 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.entity.Provinsi;
+import com.hendisantika.repository.ProvinsiRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-wilayah-indonesia
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/04/22
+ * Time: 07.16
+ */
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+    private ProvinsiRepository provinsiRepository;
+
+    public String index(Model model) {
+        List<Provinsi> provinsiList = provinsiRepository.findAll();
+        model.addAttribute("provinsiList", provinsiList);
+        return "index";
+    }
+}
