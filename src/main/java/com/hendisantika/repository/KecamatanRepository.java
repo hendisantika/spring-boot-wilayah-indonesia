d1e1babee69099efa73a60236f390f7862f81641
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:15:16 +0700

    #17 - Add KecamatanRepository

---
+++ src/main/java/com/hendisantika/repository/KecamatanRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Kecamatan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-wilayah-indonesia
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/04/22
+ * Time: 07.14
+ */
+public interface KecamatanRepository extends JpaRepository<Kecamatan, String> {
+}
