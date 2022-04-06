b5920ebb3d52cb97a2cdb1f444faa53a8daaf351
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:14:34 +0700

    #16 - Add KotaRepository

---
+++ src/main/java/com/hendisantika/repository/KotaRepository.java
@@ -0,0 +1,16 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.entity.Kota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-wilayah-indonesia
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/04/22
+ * Time: 07.14
+ */
+public interface KotaRepository extends JpaRepository<Kota, String> {
+}
