490505cd51501c2b8c1c990fe69d66ce1e1648a7
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:22:04 +0700

    #22 - Add Student Entity

---
+++ src/main/java/com/hendisantika/entity/Student.java
@@ -0,0 +1,69 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : student-crud-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/03/22
+ * Time: 06.15
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "First name is required")
+    @NotNull(message = "FirstName can not be null!!")
+    @Column(nullable = false, name = "first_Name")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    @NotNull(message = "LastName can not be null!!")
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @NotNull(message = "Please enter birth date")
+    @Past(message = "Birth date should be less than current date!!")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate birthDate;
+
+    @NotEmpty(message = "email is required")
+    @Email
+    @Column(nullable = false, name = "email", unique = true)
+    private String email;
+
+    @NotEmpty(message = "Phone number is required")
+    @NotNull(message = "Phone can not be null!!")
+    @Size(max = 13, min = 10, message = "Mobile number should be of 10 digits")
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @NotEmpty(message = "Jurusan is required")
+    @Column(nullable = false, unique = true)
+    private String jurusan;
+}
