5689dc0bce43217e02d0b4e963d9e88d2a69fa91
Hendi Santika <hendisantika@gmail.com>
2022-04-06 07:23:02 +0700

    #25 - Add StudentService

---
+++ src/main/java/com/hendisantika/service/StudentServiceImpl.java
@@ -0,0 +1,53 @@
+package com.hendisantika.service;
+
+import com.hendisantika.entity.Student;
+import com.hendisantika.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : student-crud-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/03/22
+ * Time: 06.24
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Student> findById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Student> findByEmail(String email) {
+        return studentRepository.findByEmail(email);
+    }
+
+    @Override
+    public Student save(Student std) {
+        return studentRepository.save(std);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
