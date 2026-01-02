b76d379383543695c2d62f55926ca8fd2aaf3e17
hendisantika <hendisantika@yahoo.co.id>
2026-01-03 06:57:13 +0700

    build: fix test🫸🌀✏️📗🐧🐳

--- src/test/java/com/hendisantika/SpringBootWilayahIndonesiaApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootWilayahIndonesiaApplicationTests.java
@@ -2,12 +2,22 @@ package com.hendisantika;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:postgresql://localhost:5434/wilayah_indonesia",
+        "spring.datasource.username=yu71",
+        "spring.datasource.password=53cret",
+        "spring.jpa.hibernate.ddl-auto=validate",
+        "spring.flyway.enabled=false"
+})
 class SpringBootWilayahIndonesiaApplicationTests {
 
     @Test
     void contextLoads() {
+        // Verify that the application context loads successfully
+        // This test uses the existing Docker Compose database
     }
 
 }
